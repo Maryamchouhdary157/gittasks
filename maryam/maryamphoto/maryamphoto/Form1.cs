bbd57db07de736da297ea47548294dbaf1f8a315
hariskhan41 <hariskhan3465@gmail.com>
2019-06-17 14:22:10 +0500

    1st committ
    form1 and form 2

---
+++ maryam/maryamphoto/maryamphoto/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace maryamphoto
+{
+    public partial class Form1 : Form
+    {
+
+        public static string l1;
+        public static string l2;
+        public static string l3;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+
+            OpenFileDialog opnfd = new OpenFileDialog();
+            opnfd.Filter = "Image Files (*.jpg;*.jpeg;.*.gif;)|*.jpg;*.jpeg;.*.gif";
+            if (opnfd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(opnfd.FileName);
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            l1 = textBox1.Text;
+            l2 = textBox2.Text;
+            l3 = textBox3.Text;
+            Form2 frm2 = new Form2(pictureBox1.Image);
+
+            frm2.Show();
+
+
+
+
+
+        }
+    }
+}
---
+++ maryam/maryamphoto/maryamphoto/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace maryamphoto
+{
+    public partial class Form2 : Form
+    {
+        public Form2(Image pic1)
+        {
+            InitializeComponent();
+
+
+            pictureBox1.Image = pic1;
+
+
+
+
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+            label1.Text = Form1.l1;
+            label2.Text = Form1.l2;
+            label3.Text = Form1.l3;
+        }
+    }
+}
